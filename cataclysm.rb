c4bb84443c91947416807bc402595a678bfa3693
Sudish Joseph <sudish@gmail.com>
2013-11-20 16:34:40 -0500

    Update to latest version, 0.9.
    - Use new maintainer's repo.
    - Option to build with tileset support, using SDL.
    - Install gfx subdir into libexec for tileset support.
    - Use keg-only gettext for libintl dependency.
    - Build latest git version with --HEAD.

--- cataclysm.rb
+++ cataclysm.rb
@@ -2,18 +2,35 @@ require 'formula'
 
 class Cataclysm < Formula
   homepage 'http://www.cataclysmdda.com/'
-  url 'https://github.com/TheDarklingWolf/Cataclysm-DDA/archive/0.5.zip'
-  sha1 '91250b0855cc59e1f4f93e59871a1a12bdfa1024'
+  url  'https://github.com/CleverRaven/Cataclysm-DDA/archive/0.9.zip'
+  sha1 'f2b3540618a901dadf0f8d375f6449365c7bd2ac'
+  head 'https://github.com/CleverRaven/Cataclysm-DDA.git'
+
+  option 'with-tiles', 'Include support for graphical tilesets'
+
+  depends_on 'gettext'
+  if build.with? 'tiles'
+    depends_on 'sdl'
+    depends_on 'sdl_image'
+    depends_on 'sdl_ttf'
+  end
 
   def install
-    system "make", "NATIVE=osx", "CXX=#{ENV.cxx}", "LD=#{ENV.cxx}",
-      "CXXFLAGS=#{ENV.cxxflags}"
+    cata_options = ['NATIVE=osx', 'RELEASE=1']
+    if build.with? 'tiles'
+      cata_options << 'TILES=1'
+      inreplace 'Makefile' do |s|
+        s.change_make_var! 'TILESTARGET', 'cataclysm'
+      end
+    end
+
+    system 'make', "CXX=#{ENV.cxx}", "LD=#{ENV.cxx}", *cata_options
 
     # no make install, so we have to do it ourselves
-    libexec.install "cataclysm", "data"
-    inreplace "cataclysm-launcher" do |s|
+    libexec.install 'cataclysm', 'data', 'gfx'
+    inreplace 'cataclysm-launcher' do |s|
       s.change_make_var! 'DIR', libexec
     end
-    bin.install "cataclysm-launcher" => "cataclysm"
+    bin.install 'cataclysm-launcher' => 'cataclysm'
   end
 end
